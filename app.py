52d3caf584d0d4a57ab7974b7213180667be9477
Abhijay0306 <120000614+Abhijay0306@users.noreply.github.com>
2025-07-15 14:16:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Tuberculosis Diagnosis Web App with Streamlit (Updated for Your Dataset)
 # =====================================================================
-
+import sklearn
 import pandas as pd
 import numpy as np
 import streamlit as st
